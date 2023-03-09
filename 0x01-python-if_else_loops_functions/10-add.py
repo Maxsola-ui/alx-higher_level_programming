92e3c342da01b78ed1c870c5bc576b96093726c4
Maxsola-ui <Fakinbule@gmail.com>
2023-03-09 00:13:07 -0800

    if else alx python tasks

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Fidelis Akinbule
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+i#!/usr/bin/python3
+# Author - Fidelis Akinbule
+
+def islower(c):
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
